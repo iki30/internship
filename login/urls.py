9793d31a91932e76672fa640a251844905f7e748
Иван <kachmar123@yandex.ru>
2017-10-23 01:46:25 +0400

    Initial commit

---
+++ login/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'login'
+urlpatterns = [
+    url(r'^home/$', views.home, name='home'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register/success/$', views.register_success, name='register_success'),
+    url(r'^logout/$', views.logout_page, name='logout'),
+]
